d5c4f29138f9977a447f47f961c5840cd1a3a212
MNandor <mnandor@users.noreply.github.com>
2024-06-30 14:06:54 +0300

    improve UI of history screen

--- app/src/main/java/pro/nandor/appthatchecklanguages/tabs/ListOfPastWords.kt
+++ app/src/main/java/pro/nandor/appthatchecklanguages/tabs/ListOfPastWords.kt
@@ -1,13 +1,24 @@
 package pro.nandor.appthatchecklanguages.tabs
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pro.nandor.appthatchecklanguages.Lexeme
 import pro.nandor.appthatchecklanguages.MainViewModel
 
 @Composable
@@ -15,7 +26,40 @@ fun ListOfPastWords(viewModel: MainViewModel){
     val words by viewModel.words.collectAsState()
     LazyColumn(modifier = Modifier.fillMaxSize()){
         items(words){
-            Text("${it.foreignWord} - ${it.englishWord}")
+            LexemeOnScreen(lexeme = it)
         }
     }
+}
+
+@Composable
+fun LexemeOnScreen(lexeme: Lexeme){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column(modifier = Modifier.fillMaxWidth().padding(8.dp)){
+            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()){
+                Text(lexeme.englishWord)
+                Text(lexeme.foreignWord)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(lexeme.foreignContext)
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewLexeme(){
+    val lexeme = Lexeme().apply {
+        this.englishWord = "dog"
+        this.foreignWord = "der Hund, die Hunde"
+        this.foreignContext = "Der Hund lauft schnell!"
+        this.language = "German"
+        this.exportTimeStamp = 0
+    }
+
+    LexemeOnScreen(lexeme = lexeme)
 }

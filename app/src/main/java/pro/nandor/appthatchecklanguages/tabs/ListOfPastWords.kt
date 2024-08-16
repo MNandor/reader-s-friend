31d0e847cdcde789c22dbcb8b071837cdf54b870
MNandor <mnandor@users.noreply.github.com>
2024-08-16 13:30:55 +0300

    history page - auto scroll to bottom

--- app/src/main/java/pro/nandor/appthatchecklanguages/tabs/ListOfPastWords.kt
+++ app/src/main/java/pro/nandor/appthatchecklanguages/tabs/ListOfPastWords.kt
@@ -1,5 +1,6 @@
 package pro.nandor.appthatchecklanguages.tabs
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -21,8 +23,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -67,7 +71,15 @@ fun ListOfPastWords(viewModel: MainViewModel){
                 }
 
             }
-            LazyColumn(modifier = Modifier.fillMaxSize()){
+            // auto scroll to bottom
+            val listState = remember { LazyListState() }
+            // This effect will trigger whenever the size of words changes
+            LaunchedEffect(words.size) {
+                if (words.isNotEmpty()) {
+                    listState.animateScrollToItem(words.size - 1)
+                }
+            }
+            LazyColumn(modifier = Modifier.fillMaxSize(), state = listState){
                 items(words){
                     LexemeOnScreen(lexeme = it, callBack)
                 }

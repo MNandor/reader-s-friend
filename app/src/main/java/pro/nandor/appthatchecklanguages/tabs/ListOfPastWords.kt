6cd1fdb0d2e1e683299903c28e5da6dd60526fe0
MNandor <mnandor@users.noreply.github.com>
2024-08-16 13:52:44 +0300

    improve design of lexemes in history page

--- app/src/main/java/pro/nandor/appthatchecklanguages/tabs/ListOfPastWords.kt
+++ app/src/main/java/pro/nandor/appthatchecklanguages/tabs/ListOfPastWords.kt
@@ -6,17 +6,21 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -113,10 +118,28 @@ fun LexemeOnScreen(lexeme: Lexeme, callBack: (Lexeme) -> Unit){
         Column(modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)){
-            Text(lexeme.language+"${lexeme.exportTimeStamp}", fontSize = 8.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
-            Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()){
-                Text(lexeme.englishWord)
-                Text(lexeme.foreignWord)
+            if (BuildConfig.DEBUG)
+            Text("${lexeme.language} | ${lexeme.exportTimeStamp}", fontSize = 8.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Min) // required for the divider
+            )
+            {
+                Text(lexeme.englishWord,
+                    modifier=Modifier.weight(1.0f) // take as much space as the other text
+                )
+                Divider(
+                    modifier=Modifier.fillMaxHeight().width(2.dp),
+                    thickness = 2.dp,
+                    color = Color.Gray
+                )
+                Text(lexeme.foreignWord,
+                    modifier=Modifier.weight(1.0f),
+                    textAlign = TextAlign.Right
+                )
+
             }
             Spacer(modifier = Modifier.height(8.dp))
             TextThatHighlights(lexeme.foreignContext)

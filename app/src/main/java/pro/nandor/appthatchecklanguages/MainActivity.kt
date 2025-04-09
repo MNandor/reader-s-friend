3c2276b6ae87d22e4f7e27195052ee780c0b3fc0
MNandor <mnandor@users.noreply.github.com>
2025-04-10 00:33:27 +0300

    fix viewmodel not surviving configuration changes and thus resetting language selection

--- app/src/main/java/pro/nandor/appthatchecklanguages/MainActivity.kt
+++ app/src/main/java/pro/nandor/appthatchecklanguages/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -64,7 +65,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val viewModel: MainViewModel = MainViewModel()
+        val viewModel: MainViewModel by viewModels()
 
         setContent {
             AppThatChecksLanguagesTheme {
